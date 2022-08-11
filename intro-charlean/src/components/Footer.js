cd9a6007c2155ad85d8758c0c79759d1ed63c296
Charlean Baxter <baxtercm@outlook.com>
2022-08-11 01:03:12 -0700

    fav things react app

---
+++ intro-charlean/src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <>
+      <h3>2022 LEARN | Syntactical Astronaut</h3>
+    </>
+  )
+}
+
+export default Footer
---
+++ intro-charlean/src/components/Profile.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Profile = () => {
+  return (
+    <ul>
+      <h2>About the Syntactical Astronaut</h2>
+      <li>Name: Charlean Baxter </li>
+      <li>aliases: "Syntactical Magician", "Mom", "Can you help me find...", "Mee Maw" </li>
+      <li>backstory: Became a full-stack web developer under the tutelage of LEARN Academy. Currently my role as an Associate Instructor at LEARN supports my Amazon investment and exposes me to some purposeful, creative, compassionate engineers. </li>
+      <li>daily goal: Giving back to the community and influencing others to celebrate each phase of their web development journey.</li>
+    </ul>
+  )
+}
+
+export default Profile
